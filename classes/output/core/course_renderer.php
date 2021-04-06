e2388d757cf0f463294f6d965f9625da2e15eed0
BELACHHAB-MOHAMMED <belmedbsns@gmail.com>
2021-04-06 11:02:34 +0100

    Big Updates

--- classes/output/core/course_renderer.php
+++ classes/output/core/course_renderer.php
@@ -135,13 +135,13 @@ class course_renderer extends \core_course_renderer {
         }
 
         $coursecount = 1;
-        $content .= html_writer::start_tag('div', array('class' => 'card-deck mt-2'));
+        $content .= html_writer::start_tag('div', array('class' => 'card-deck mt-2 cd-cr'));
         foreach ($courses as $course) {
             $content .= $this->coursecat_coursebox($chelper, $course);
 
             if ($coursecount % 4 == 0) {
                 $content .= html_writer::end_tag('div');
-                $content .= html_writer::start_tag('div', array('class' => 'card-deck mt-2'));
+                $content .= html_writer::start_tag('div', array('class' => 'card-deck mt-2 cd-cr'));
             }
 
             $coursecount ++;
